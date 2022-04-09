b1e7d963052b0a54e76b7614d74f368541084ece
ArjanaaTernava <94221223+ArjanaaTernava@users.noreply.github.com>
2022-04-10 00:15:22 +0200

    Updated client.js

--- client.js
+++ client.js
@@ -1,1 +1,55 @@
+var dgram = require('dgram');
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.question('Execute command: ', (cmd) => {
+
+    const user = {
+        username: 'user',
+        password: 'password',
+        permissions: {
+          read: true,
+          write: false,
+          execute: false,
+        },
+        details() {
+          const { read, write, execute } = this.permissions;
+          return [this.username, this.password, read, write, execute].join(" ");
+        },
+      };
+  
+
+
+
+let PORT = 8882;
+let HOST = '127.0.0.1' ;
+
+// Buffers in Node.js is used to perform operations on raw binary data
+
+var message = new Buffer(`${cmd}: ` + user.details());
+
+// Create a udp socket client object.
+let client = dgram.createSocket('udp4');
+
+client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
+
+    if (err) throw err;
+
+    console.log('UDP client message sent to ' + HOST + ':' + PORT);
+
+});
+
+client.on('message', function (message, remote) {
+
+    console.log(remote.address + ':' + remote.port + ' - ' + message);
+
+    client.close();
+
+});
+
+  readline.close();
+});
 
